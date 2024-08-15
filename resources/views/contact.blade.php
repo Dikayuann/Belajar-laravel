b6568161ec5e1db7b5f01adfea45c6d59c8467bc
Dika Yuan <dikayuanfatma21@gmail.com>
2024-08-15 10:28:35 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Halaman Contact</h3>
+    <ul>
+        <li>
+            <a href="https://instagram.com">Instragram</a>
+        </li>
+        <li>
+            <a href="https://x.com">X</a>
+        </li>
+        <li>
+            <a href="https://youtube.com">YouTube</a>
+        </li>
+    </ul>
+</x-layout>
